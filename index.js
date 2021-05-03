410d7d50348ebdcd9885757cdfadb849716bc3c2
Gabriel Lopez <gabelopez84@gmail.com>
2021-05-03 13:45:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const divide = (parameter1, parameter2) => parameter1 / parameter2;
-divide(100,2000); //=> 20 
+const divide = (parameter1, parameter2) => 2000 / 100;
+divide(2000, 100); //=> 20 
+
+const square = (parameter1) => {
+    
+    return parameter1 * parameter1; 
+
+}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5 
